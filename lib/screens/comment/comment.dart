0e7e68546d76f01d08310871b5949c57c77f1213
alireza <shehabalireza@gmail.com>
2025-01-09 00:16:09 -0800

    set interceptor

--- lib/screens/comment/comment.dart
+++ lib/screens/comment/comment.dart
@@ -1,8 +1,10 @@
+import 'package:cherry_toast/cherry_toast.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:online_shop/data/model/product_detail_model.dart';
 import 'package:online_shop/screens/comment/bloc/comment_bloc.dart';
+import 'package:online_shop/screens/productDetail/bloc/prodcut_detail_bloc.dart';
 
 class CommentScreen extends StatefulWidget {
   final int productId;
@@ -19,30 +21,48 @@ class _CommentScreenState extends State<CommentScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: Container(
-        height: 85,
-        padding: EdgeInsets.fromLTRB(
-          16,
-          15,
-          16,
-          25,
-        ),
-        child: ElevatedButton(
-          onPressed: () {
-            if (FormKey.currentState!.validate()) {
-              BlocProvider.of<CommentBloc>(context).add(CommentSendEvent(
-                  comments: Comments(
-                      subject: subjectController.text,
-                      id: widget.productId,
-                      text: textController.text)));
-
-              print('salam');
-            }
-          },
-          child: Text(
-            'ثبت نظر',
-          ),
-        ),
+      bottomNavigationBar: BlocConsumer<CommentBloc, CommentState>(
+        listener: (context, state) {
+          if (state is CommentSuccessState) {
+            BlocProvider.of<ProductDetailBloc>(context).add(
+              ProductDetailEventInit(
+                id: widget.productId,
+              ),
+            );
+            CherryToast.success(
+              toastDuration: Duration(seconds: 2),
+              title: Text(
+                'نظر شما با موفقیت ثبت شد',
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+            ).show(context);
+          }
+        },
+        builder: (context, state) {
+          return Container(
+            height: 85,
+            padding: EdgeInsets.fromLTRB(
+              16,
+              15,
+              16,
+              25,
+            ),
+            child: ElevatedButton(
+              onPressed: () {
+                if (FormKey.currentState!.validate()) {
+                  BlocProvider.of<CommentBloc>(context).add(CommentSendEvent(
+                      comments: Comments(
+                          subject: subjectController.text,
+                          productId: widget.productId,
+                          text: textController.text)));
+                }
+              },
+              child: Text(
+                'ثبت نظر',
+              ),
+            ),
+          );
+        },
       ),
       appBar: AppBar(
         title: Text(
--- lib/utils/http_clinet.dart
+++ lib/utils/http_clinet.dart
@@ -1,7 +1,18 @@
 import 'package:dio/dio.dart';
+import 'package:online_shop/main.dart';
 
 Dio httpClient = Dio(
   BaseOptions(
     baseUrl: 'https://flutter.vesam24.com/api/',
   ),
-);
+)..interceptors.add(InterceptorsWrapper(
+    onError: (error, handler) {},
+    onRequest: (options, handler) {
+      String? userToke = prefs.getString('token');
+      print('user toke $userToke');
+      if (userToke != null && isLogin.value) {
+        options.headers['Authorization'] = 'Bearer $userToke';
+      }
+      handler.next(options);
+    },
+  ));
